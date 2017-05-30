5b5d3bd9556a6d8bbcc0b4158e22246b5b43ff59
brunkovskyi <brunkovskyi@ngoar.net>
2017-05-30 19:15:54 +0300

    added Hello to Main class

--- src/Main/java/Main/Main.java
+++ src/Main/java/Main/Main.java
@@ -5,6 +5,7 @@ import Data.DataWorker;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello");
         DataWorker dataWorker = new DataWorker();
         HedgehogField field = dataWorker.readInputField();
         int result = field.getResult();
